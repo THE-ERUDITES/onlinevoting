f2bf5706516765983efb81850c8747564f98e31b
Riya <riyapsinghal@gmail.com>
2020-11-25 04:47:54 +0530

    statistics

--- statistics.php
+++ statistics.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+$db_host = 'localhost';
+    $db_username = 'root';
+    $db_password = '';
+    $db_name = 'onlinevoting';
+    $conn = mysqli_connect($db_host,$db_username,$db_password,$db_name);
 ?>
 
 <!DOCTYPE html>
@@ -65,6 +70,176 @@ session_start();
         </div>
       </div> 
     </nav>
-    <!-- end of container -->
+    <!-- end of container --><br><br><br>
+
+<?php
+ 
+ //--------------Pie Chart ---------------------
+$dataPointsPie = array();
+$totalvotes=0;
+
+$sql = "Select * from candidate";
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_assoc($result)) { 
+  $totalvotes= $totalvotes + $row["count_vote"];
+}
+
+$sql = "Select * from candidate";
+$result1 = mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_assoc($result1)) { 
+  $percent = $row["count_vote"]*100/$totalvotes;
+  $label="Candidate : ".$row["cname"]." , Party : ".$row["party"];
+  array_push($dataPointsPie, array("label"=> $label, "y"=> $percent));
+}
+
+//-------------Bar Graph-----------------//
+$sql = "Select * from candidate";
+$result2 = mysqli_query($conn,$sql);
+
+$dataPointsBar = array(); 
+
+while($row = mysqli_fetch_assoc($result2)) { 
+  $label="Party : ".$row["party"];
+  array_push($dataPointsBar, array("y"=> $row["count_vote"] ,"label"=> $label ));
+}
+
+
+//-------------Line Graph-----------------//
+$sql = "Select * from voter where flag=1";
+$result3 = mysqli_query($conn,$sql);
+
+$cnt=0;
+while($row = mysqli_fetch_assoc($result3)) { 
+  $cnt++;
+}
+
+//just to increase scale
+$cnt=$cnt*5;
+$cnt=76+$cnt;
+
+$dataPointsLine = array(
+	array("y" => 65, "label" => "1995"),
+	array("y" => 89, "label" => "2000"),
+	array("y" => 101, "label" => "2005"),
+	array("y" => 84, "label" => "2010"),
+	array("y" => 113, "label" => "2015"),
+	array("y" => $cnt, "label" => "2020")
+);
+	
+?>
+ 
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+//Pie Chart
+window.onload = function() {
+  
+var chartPie = new CanvasJS.Chart("chartContainerPie", {
+	animationEnabled: true,
+	title: {
+		text: "Percentage of Votes Obtained"
+	},
+	subtitles: [{
+		text: "November 2020"
+	}],
+	data: [{
+		type: "pie",
+		yValueFormatString: "#,##0.00\"%\"",
+		indexLabel: "{label} ({y})",
+		dataPoints: <?php echo json_encode($dataPointsPie, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chartPie.render();
+
+
+//Bar Graph
+ var chartBar = new CanvasJS.Chart("chartContainerBar", {
+   animationEnabled: true,
+   title:{
+     text: "ELECTION GROWTH"
+   },
+   subtitles: [{
+		text: "November 2020"
+	}],
+   axisY: {
+     title: "",
+     includeZero: true,
+     prefix: "",
+     suffix:  ""
+   },
+   data: [{
+     type: "bar",
+     yValueFormatString: "#,## votes",
+     indexLabel: "{y}",
+     indexLabelPlacement: "inside",
+     indexLabelFontWeight: "bolder",
+     indexLabelFontColor: "white",
+     dataPoints: <?php echo json_encode($dataPointsBar, JSON_NUMERIC_CHECK); ?>
+   }]
+ });
+ chartBar.render();
+
+
+ //line chart
+var chartLine = new CanvasJS.Chart("chartContainerLine", {
+  animationEnabled: true,
+	title: {
+		text: "Voters Turnout in Indian Elections"
+	},
+	axisY: {
+		title: "Number of Citizens(in millions)"
+	},
+	data: [{
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPointsLine, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+
+chartLine.render();
+ 
+}
+
+</script>
+</head>
+<body>
+<div class="container" style="padding:30px 30px 30px 30px ;">
+      <div class="row">
+        <div class="col-sm-12" style="border:2px solid gray;">
+        <center>
+          <br>
+            <div id="chartContainerPie" style="height: 400px; width: 100%;"></div>
+          <br>
+        </center>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+        <div class="col-sm-12" style="border:2px solid gray;">
+        <center>
+          <br>
+         <div id="chartContainerBar" style="height: 400px; width: 80%;"></div>
+          <br>
+        </center>
+        </div>
+      </div><br><br>
+      <div class="row">
+        <div class="col-sm-12" style="border:2px solid gray;">
+        <center>
+          <br>
+            <!--no of people who voted - flag=1-->
+            <div id="chartContainerLine" style="height: 400px; width: 80%;"></div><br>
+            <h2 class="normalFont" style="font-size:15px" ></span>Number of Indian Citizens voting in this year's election as compared to Previous Year's Turnouts<br>(previous year data is not true to knowledge and is assumed accordingly)</h2>
+          <br>
+        </center>
+        </div>
+      </div>
+</div>
+
+
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>   
 
     
