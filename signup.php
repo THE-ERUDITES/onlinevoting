d13ddd80d49054012e454b531cf94033f4911c39
yamini-kabra <yaminikabra2001@gmail.com>
2020-11-24 16:53:41 +0530

    add changes to signup.php

--- signup.php
+++ signup.php
@@ -108,44 +108,104 @@ flag => flag -->
 <html>
 <head>
     <!-- <link rel="stylesheet" href="style.css"> -->
-    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+
+    <style>
+      .headerFont{
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 24px;
+      }
+
+      .subFont{
+        font-family: 'Raleway', sans-serif;
+        font-size: 14px;
+        
+      }
+      
+      .specialHead{
+        font-family: 'Oswald', sans-serif;
+      }
+
+      .normalFont{
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+    </style>
 </head>
 <body>
-	<h1><u>Sign Up</u></h1>
+<div class="container">
+  	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+        <div class="navbar-header">
+          <a href="index.html" class="navbar-brand headerFont text-lg"><strong>eVoting</strong></a>
+      </div>
+    </nav>
 
-	<form action="signup.php" method="post">
+    
+    <div class="container" style="padding-top:150px;">
+    	<div class="row">
+    		<div class="col-sm-4"></div>
+    		<div class="col-sm-4" style="border:2px solid gray;padding:50px;">
+    			
+    			<div class="page-header">
+    				<h2 class="specialHead">Sign up</h2>
+                </div>
+                
+          <form action="signup.php" method="POST">
+      			<div class="form-group">
+                      
+                      <label>Full Name:</label>
+                        <input type="text" name="name" placeholder="Full Name" class="form-control" value="<?php echo isset($_POST["name"]) ? $_POST["name"] : ''; ?>" required>
+                        <div style="color: red;"><?php echo $errors['name']; ?></div><br><br>
 
-		<label>Full Name:</label>
-		<input type="text" name="name" placeholder="Full Name" value="<?php echo isset($_POST["name"]) ? $_POST["name"] : ''; ?>" required>
-		<div style="color: red;"><?php echo $errors['name']; ?></div><br><br>
+                        <label>Age:</label>
+                        <input type="int" name="age" class="form-control" value="<?php echo isset($_POST["age"]) ? $_POST["age"] : ''; ?>" required>
+                        <div style="color: red;"><?php echo $errors['age']; ?></div><br><br>
 
-		<label>Age:</label>
-		<input type="int" name="age" value="<?php echo isset($_POST["age"]) ? $_POST["age"] : ''; ?>" required>
-		<div style="color: red;"><?php echo $errors['age']; ?></div><br><br>
+                        <label>Username:</label>
+                        <input type="text" name="uname" class="form-control" value="<?php echo isset($_POST["uname"]) ? $_POST["uname"] : ''; ?>" required>
+                        <div style="color: red;"><?php echo $errors['uname']; ?></div><br><br>
 
-		<label>Username:</label>
-		<input type="text" name="uname" value="<?php echo isset($_POST["uname"]) ? $_POST["uname"] : ''; ?>" required>
-		<div style="color: red;"><?php echo $errors['uname']; ?></div><br><br>
+                        <label>Email Id:</label>
+                        <input type="email" name="email" class="form-control" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>" required>
+                        <br><br>
 
-		<label>Email Id:</label>
-		<input type="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>" required>
-		<br><br>
+                        <label>Voter ID:</label>
+                        <input type="int" name="voter_id" class="form-control" value="<?php echo isset($_POST["voter_id"]) ? $_POST["voter_id"] : ''; ?>" pattern="[0-9]{7}" required><br><br>
+                        <div style="color: red;"><?php echo $errors['voter_id']; ?></div>   
+                        <label>Password:</label>
+                        <input type="password" name="password" class="form-control" placeholder="8-50 characters" required>
+                        <div style="color: red;"><?php echo $errors['password']; ?></div><br><br>
 
-		<label>Voter ID:</label>
-		<input type="int" name="voter_id" value="<?php echo isset($_POST["voter_id"]) ? $_POST["voter_id"] : ''; ?>" pattern="[0-9]{7}" required><br><br>
-        <div style="color: red;"><?php echo $errors['voter_id']; ?></div><br><br>
-		<label>Password:</label>
-		<input type="password" name="password" placeholder="8-50 characters" required>
-		<div style="color: red;"><?php echo $errors['password']; ?></div><br><br>
+                        <label>Confirm Password:</label>
+                        <input type="password" name="cpassword" class="form-control" required>
+                        <div style="color: red;"><?php echo $errors['cpassword']; ?></div><br><br>
 
-		<label>Confirm Password:</label>
-		<input type="password" name="cpassword" required>
-		<div style="color: red;"><?php echo $errors['cpassword']; ?></div><br><br>
+      				<button type="submit" name="submit" class="btn btn-block span btn-primary "><span class="glyphicon glyphicon-user"></span> Sign In</button>
+                    <br><a class="form-control" href="login.php">Already an user? Log in!</a>
+      			</div>
 
-		<input type="submit" name="submit" value="Submit">
+          </form>
+          <br>
 
-	</form>
-    <h3><a href="login.php">Already an user?Log in!</a></h3>
+    		</div>
+    		<div class="col-sm-4"></div>
+    	</div>
+    </div>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    
     
 </body>
 </html>
